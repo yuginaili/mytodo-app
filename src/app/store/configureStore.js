ebd9510855e123949b57e8f35cdac4776759a89e
Yugi Naili <yugi.naili@gmail.com>
2017-10-13 17:55:39 -0400

    First commit

---
+++ src/app/store/configureStore.js
@@ -0,0 +1,28 @@
+import {compose, createStore} from 'redux';
+import rootReducer from '../reducers';
+
+import persistState, {mergePersistedState} from 'redux-localstorage';
+import adapter from 'redux-localstorage/lib/adapters/localStorage';
+
+export default function configureStore(initialState) {
+  const reducer = compose(
+    mergePersistedState()
+  )(rootReducer, initialState);
+
+  const storage = adapter(window.localStorage);
+
+  const createPersistentStore = compose(
+    persistState(storage, 'state')
+  )(createStore);
+
+  const store = createPersistentStore(reducer);
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextReducer = require('../reducers').default;
+      store.replaceReducer(nextReducer);
+    });
+  }
+
+  return store;
+}
